faee73304a80e0e8147cf61f5c5d45a7775202c2
Noah Hill <noahhill576@aol.com>
2025-09-01 04:00:39 +0700

    deploy 1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Thêm cấu hình cho GitHub Pages
+  output: 'export',
   images: {
+    unoptimized: true, // GitHub Pages không hỗ trợ tối ưu hóa hình ảnh của Next.js
     remotePatterns: [
       {
         protocol: 'https',
@@ -10,6 +13,10 @@ const nextConfig = {
       },
     ],
   },
+  // Nếu repository của bạn có tên khác với tên người dùng GitHub, bạn cần thêm cấu hình basePath:
+  // basePath: '/repository-name',
+  // Nếu bạn cần cấu hình assetPrefix:
+  // assetPrefix: '/repository-name',
 }
 
 module.exports = nextConfig
